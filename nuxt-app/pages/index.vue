5326d1c87905b425a88dafcab4e3a8d970c84130
Prashannjeet Paul <prashannjeet.paul@olacabs.com>
2022-08-01 21:56:15 +0530

    login Page

--- nuxt-app/pages/index.vue
+++ nuxt-app/pages/index.vue
@@ -2,7 +2,8 @@
 <div>
 <p>Nuxt Crud</p>
 <div>
-    <nuxt-link to="sign">SignUp</nuxt-link>
+    <nuxt-link to="sign">SignUp</nuxt-link><br><br>
+    <nuxt-link to="login">Login</nuxt-link>
 </div>
 </div>
 </template>
---
+++ nuxt-app/pages/login.vue
@@ -0,0 +1,31 @@
+<template>
+<fieldset>
+    <legend>
+        <h2>LogIn</h2>
+    </legend>
+    <form id="logIn">
+        <input type="email" id="email" name="email" placeholder="Email"><br><br>
+        <input type="password" id="password" name="password" placeholder="password"><br><br>
+        <button @click="logIn">LogIn</button>
+    </form>
+</fieldset>
+</template>
+<script></script>
+<style>
+#logIn input{
+    width: 300px;
+    height: 40px;
+    padding-left: 20px;
+    margin-bottom: 30px;
+    margin-left: auto;
+    margin-right: auto;
+    border: 1px solid skyblue;
+}
+#logIn button{
+    width: 320px;
+    height: 40px;
+    border: 1px solid skyblue;
+    color: #fff;
+    background-color: skyblue;
+    cursor: pointer;
+}</style>
