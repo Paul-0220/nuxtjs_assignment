61dab12b307dca2d0c608d0dc1682c80369ab17d
Prashannjeet Paul <prashannjeet.paul@olacabs.com>
2022-08-01 20:20:09 +0530

    signup layout

---
+++ nuxt-app/pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+<div>
+<p>Nuxt Crud</p>
+<div>
+    <nuxt-link to="sign">SignUp</nuxt-link>
+</div>
+</div>
+</template>
---
+++ nuxt-app/pages/sign.vue
@@ -0,0 +1,25 @@
+<template>
+<fieldset>
+    <legend>
+        <h2>SignUp</h2>
+    </legend>
+    <form id="signUp">
+        <input type="text" id="userName" name="userName" placeholder="User Name"><br><br>
+                <input type="email" id="email" name="email" placeholder="Email"><br><br>
+                        <input type="password" id="password" name="password" placeholder="password"><br><br>
+                        <button>Sign Up</button>
+    </form>
+</fieldset>
+</template>
+<script></script>
+<style>
+#signUp input{
+width: 300px;
+height: 40px;
+padding-left: 20px;
+margin-bottom: 30px;
+margin-left: auto;
+margin-right: auto;
+border:1px solid skyblue
+}
+</style>
